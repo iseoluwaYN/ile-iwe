7b7860fecc9eb94ce69bfa906d80434c8b630402
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-10-29 18:04:52 +0100

    Ileiwe (Learning party service class)

--- src/main/java/com/ileiwe/ileiwe/security/user/LearningPartyServiceImpl.java
+++ src/main/java/com/ileiwe/ileiwe/security/user/LearningPartyServiceImpl.java
@@ -1,2 +1,37 @@
-package com.ileiwe.ileiwe.security.user;public class LearningPartyServiceImpl {
+package com.ileiwe.ileiwe.security.user;
+
+import com.ileiwe.ileiwe.data.model.LearningParty;
+import com.ileiwe.ileiwe.data.repository.LearningPartyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LearningPartyServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private LearningPartyRepository learningPartyRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        LearningParty user=
+            learningPartyRepository.findByEmail(s);
+        if(user ==null){
+            throw new UsernameNotFoundException("User with email does not exist");
+        }
+        List<SimpleGrantedAuthority> authorities = user.getAuthorities()
+                .stream().map(authority -> new SimpleGrantedAuthority(authority.getAuthority().name()))
+                .collect(Collectors.toList());
+
+        return new User(user.getEmail(), user.getPassword(), authorities);
+    }
 }
