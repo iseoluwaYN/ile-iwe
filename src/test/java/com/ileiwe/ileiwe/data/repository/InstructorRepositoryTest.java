11e1228f3e1b2dedc9ae3e4a37f2193a4e990def
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2021-10-29 14:42:13 +0100

    Tests

--- src/test/java/com/ileiwe/ileiwe/data/repository/InstructorRepositoryTest.java
+++ src/test/java/com/ileiwe/ileiwe/data/repository/InstructorRepositoryTest.java
@@ -1,4 +1,116 @@
+package com.ileiwe.ileiwe.data.repository;
+
+import com.ileiwe.ileiwe.data.model.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@Sql( scripts = {"/db/insert.sql"})
 class InstructorRepositoryTest {
-  
+
+
+    @Autowired
+    InstructorRepository instructorRepository;
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void saveInstructorAsLearningPartyTest(){
+//        create a learning party
+        LearningParty user = new LearningParty("trainer@ileiwe.com"
+                , "password", new Authority(Role.ROLE_INSTRUCTOR));
+//        create instructor
+        Instructor instructor = Instructor.builder()
+                .firstName("John").lastName("Doe")
+                .learningParty(user).build();
+//        map instruc
+        log.info("Before saving ->{}", instructor);
+        instructorRepository.save(instructor);
+        assertThat(instructor.getId()).isNotNull();
+        assertThat(instructor.getLearningParty().getId()).isNotNull();
+        log.info("After saving ->{}", instructor);
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void updateInstructorTableAfterCreate(){
+//        create instructor
+
+        LearningParty user = new LearningParty("trainer@ileiwe.com"
+                , "password", new Authority(Role.ROLE_INSTRUCTOR));
+//        create instructor
+        Instructor instructor = Instructor.builder()
+                .firstName("John").lastName("Doe")
+                .learningParty(user).build();
+//        map instructor
+        log.info("Before saving ->{}", instructor);
+
+        instructorRepository.save(instructor);
+        assertThat(instructor.getId()).isNotNull();
+        assertThat(instructor.getLearningParty().getId()).isNotNull();
+        log.info("After saving ->{}", instructor);
+
+        Instructor savedInstructor=
+            instructorRepository.findById(instructor.getId()).orElse(null);
+
+        assertThat(savedInstructor).isNotNull();
+        assertThat(savedInstructor.getBio()).isNull();
+        assertThat(savedInstructor.getGender()).isNull();
+
+        savedInstructor.setBio("I am a java lord");
+        savedInstructor.setGender(Gender.FEMALE);
+
+        instructorRepository.save(savedInstructor);
+        assertThat(savedInstructor.getBio()).isNotNull();
+        assertThat(savedInstructor.getGender()).isNotNull();
+    }
+
+    @Test
+    void createInstructorWithNullValueTest(){
+//        create a learning party
+        LearningParty user = new LearningParty("trainer@ileiwe.com"
+                , "password", new Authority(Role.ROLE_INSTRUCTOR));
+//        create instructor with null values
+        Instructor instructor = Instructor.builder()
+                .firstName(null).lastName(null)
+                .learningParty(user).build();
+
+        assertThrows(ConstraintViolationException.class,() -> instructorRepository.save(instructor));
+    }
+
+    @Test
+    void createInstructorWithEmptyStringValueTest(){
+//        create a learning party
+        LearningParty user = new LearningParty("trainer@ileiwe.com"
+                , "password", new Authority(Role.ROLE_INSTRUCTOR));
+//        create instructor with null values
+        Instructor instructor = Instructor.builder()
+                .firstName(" ").lastName(" ")
+                .learningParty(user).build();
+
+        assertThrows(ConstraintViolationException.class,() -> instructorRepository.save(instructor));
+    }
+
 }
--- src/test/java/com/ileiwe/ileiwe/data/repository/LearningPartyRepositoryTest.java
+++ src/test/java/com/ileiwe/ileiwe/data/repository/LearningPartyRepositoryTest.java
@@ -1,4 +1,102 @@
+package com.ileiwe.ileiwe.data.repository;
+
+import com.ileiwe.ileiwe.data.model.Authority;
+import com.ileiwe.ileiwe.data.model.LearningParty;
+import com.ileiwe.ileiwe.data.model.Role;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.useRepresentation;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+@Transactional
+@Sql(scripts = {"/db/insert.sql"})
 class LearningPartyRepositoryTest {
-  
+
+    @Autowired
+    LearningPartyRepository learningPartyRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void createdLearningPartyTest(){
+        LearningParty learningUser = new LearningParty("my@gmail.com"
+                , "mine123",
+                new Authority(Role.ROLE_STUDENT));
+        learningPartyRepository.save(learningUser);
+        assertThat(learningUser.getId()).isNotNull();
+        assertThat(learningUser.getEmail()).isEqualTo("my@gmail.com");
+        assertThat(learningUser.getAuthorities()
+                .get(0).getAuthority()).isEqualTo(Role.ROLE_STUDENT);
+
+        log.info("After saving ->{}", learningUser);
+    }
+
+    @Test
+    void createdLearningPartyUniqueEmailsTest(){
+//        create a learning party
+        LearningParty user1 = new LearningParty("my@gmail.com"
+                , "mine123",
+                new Authority(Role.ROLE_STUDENT));
+//        save to db
+        learningPartyRepository.save(user1);
+        assertThat(user1.getId()).isNotNull();
+        assertThat(user1.getEmail()).isEqualTo("my@gmail.com");
+//        create another learning party with some email
+        LearningParty user2 = new LearningParty("my@gmail.com"
+                , "mine123",
+                new Authority(Role.ROLE_STUDENT));
+//        save and catch exception
+        assertThrows(DataIntegrityViolationException.class,() -> learningPartyRepository.save(user2));
+//        assert that exception was thrown
+
+    }
+
+    @Test
+    void LearningPartyWithNullEmail(){
+//         create a learning party with null values
+        LearningParty user2 = new LearningParty(null,
+                null,
+                new Authority(Role.ROLE_STUDENT));
+//          save and catch exception
+        assertThrows(ConstraintViolationException.class, ()-> learningPartyRepository.save(user2));
+    }
+
+    @Test
+    void LearningPartyWithEmptyStringValue(){
+//         create a learning party with null values
+        assertThrows(ConstraintViolationException.class,() -> new LearningParty("",
+                "",
+                new Authority(Role.ROLE_STUDENT)));
+    }
+
+    @Test
+    void findUserNameTest(){
+        LearningParty learningParty =
+                learningPartyRepository.findByEmail("iseA@gmail.com");
+        assertThat(learningParty).isNotNull();
+        assertThat(learningParty.getEmail()).isEqualTo("iseA@gmail.com");
+        log.info("Learning party object ->{}", learningParty);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
