538d18fe6645d42b742b43011148ea21541fe210
Iseoluwa Fasoyin <iseoluwafasoyin@gmail.com>
2022-03-02 20:30:21 +0100

    update

--- src/main/java/com/ileiwe/ileiwe/security/config/WebSecurityConfiguration.java
+++ src/main/java/com/ileiwe/ileiwe/security/config/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.ileiwe.ileiwe.security.user.LearningPartyServiceImpl;
 import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,21 +19,20 @@ import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @EnableWebSecurity
-public class WebSecurityConfig
+public class WebSecurityConfiguration
         extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
     private final LearningPartyServiceImpl userDetails;
 
     @Autowired
-    public  WebSecurityConfig(PasswordEncoder passwordEncoder, LearningPartyServiceImpl learningPartyService){
-        this.passwordEncoder = passwordEncoder;
+    public WebSecurityConfiguration(LearningPartyServiceImpl learningPartyService){
         this.userDetails = learningPartyService;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/instructors/save").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -40,14 +40,13 @@ public class WebSecurityConfig
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .exceptionHandling()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        ;
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetails)
-                .passwordEncoder(passwordEncoder);
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
